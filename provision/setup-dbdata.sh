a8079b73e3a4079733e26420179bbb91ac1a3f61
MorganFinTW <morganboy@gmail.com>
2015-08-21 18:04:01 +0800

    modify db import script code

--- provision/setup-dbdata.sh
+++ provision/setup-dbdata.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 #
+cd /dbsql
 
-mysql -u root -pshopperDBadmin demoa6 < $1
-mysql -u root -pshopperDBadmin root < $1
+readonly DBSQL=( "demoa6.sql")
+for i in "${DBSQL[@]}"; do
+    NAME=$i
+    mysql -u root -pshopperDBadmin demoa6 < $NAME
+done
+
+
+readonly NOVELSQL=( "ip2nation.sql"  "ip2nationCountries.sql"  "shop_menu_limit.sql"  "shop_menu_limit_relation.sql" )
+for i in "${NOVELSQL[@]}"; do
+    NAME=$i
+    mysql -u root -pshopperDBadmin novellink < $NAME
+done
